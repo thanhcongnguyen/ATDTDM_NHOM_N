f3eb482bcfd220e7ddd1b2e6edb1c7e31f9aa07b
ThanhCong <thanhcong@ThanhCongs-MacBook-Pro.local>
2020-12-05 23:13:27 +0700

    add project

--- routes/products.js
+++ routes/products.js
@@ -3,42 +3,42 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     req.getConnection((err, conn) => {
-        conn.query('SELECT * FROM SanPham', (err, data) => {
+        connection.query('SELECT * FROM SanPham', (err, data) => {
          if (err) {
           res.json(err);
          }
-         res.render('SanPham', { data });
+         res.render('Products', { data });
         });
     });
 });
 
-router.post('/create', function(req, res, next) {
-    const data = req.body;
-    req.getConnection((err, connection) => {
-        conn.query('INSERT INTO SanPham set ?', data, (err, results) => {
-        res.redirect('/');
-      });
-    });
-});
+// router.post('/create', function(req, res, next) {
+//     const data = req.body;
+//     req.getConnection((err, connection) => {
+//         connection.query('INSERT INTO SanPham set ?', data, (err, results) => {
+//         res.redirect('/');
+//       });
+//     });
+// });
 
-router.get('/update/:MaSP', function(req, res, next) {
-    const MaKH = req.params.MaKH;
-    req.getConnection((err, conn) => {
-        conn.query("SELECT * FROM SanPham WHERE MaSP = ?", [MaKH], (err, rows) => {
-        res.render('SanPham_Update', {
-            data: rows[0]
-        });
-        });
-    });
-});
+// router.get('/update/:MaSP', function(req, res, next) {
+//     const MaKH = req.params.MaKH;
+//     req.getConnection((err, conn) => {
+//         connection.query("SELECT * FROM SanPham WHERE MaSP = ?", [MaKH], (err, rows) => {
+//         res.render('SanPham_Update', {
+//             data: rows[0]
+//         });
+//         });
+//     });
+// });
 
-router.get('/delete/:MaSP', function(req, res, next) {
-    const MaKH = req.params.MaKH;
-    req.getConnection((err, connection) => {
-      conn.query('DELETE FROM SanPham WHERE MaSP = ?', [MaKH], (err, rows) => {
-        res.redirect('/');
-    });
-    })
-});
+// router.get('/delete/:MaSP', function(req, res, next) {
+//     const MaKH = req.params.MaKH;
+//     req.getConnection((err, connection) => {
+//         connection.query('DELETE FROM SanPham WHERE MaSP = ?', [MaKH], (err, rows) => {
+//         res.redirect('/');
+//     });
+//     })
+// });
 
 module.exports = router;
